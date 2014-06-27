0a4b16bb1cefb0399c60ff75f82f2855b38c4e11
axelerod <aleksei.burov@gmail.com>
2014-06-27 10:59:36 +0300

    Locks prepare class.

---
+++ untitled/multithread/src/main/java/burov/locks/LockExample.java
@@ -0,0 +1,11 @@
+package burov.locks;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: alexey
+ * Date: 27.06.14
+ * Time: 10:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class LockExample {
+}
