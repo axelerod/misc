aafbfa3838f537bd88d56a15877ffda235e3a193
Oleksii Burov <aburov@smartling.com>
2015-11-17 15:54:37 +0200

    GREEN: added implementation.

--- untitled/approach/tdd/calculator/src/main/java/calculation/Calculator.java
+++ untitled/approach/tdd/calculator/src/main/java/calculation/Calculator.java
@@ -2,6 +2,7 @@ package calculation;
 
 public class Calculator {
     public int add(String numbersAsString) {
+        if ("".equals(numbersAsString)) return 0;
         return Integer.valueOf(numbersAsString);
     }
 }
