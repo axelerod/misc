03be6abb0dcfe31ad4c55b0159abeb878d228347
Oleksii Burov <aburov@smartling.com>
2015-11-17 17:18:44 +0200

    REF: removed duplication: 2 failed tests, step back, add null constraint, then apply duplication.

--- untitled/approach/tdd/calculator/src/main/java/calculation/Calculator.java
+++ untitled/approach/tdd/calculator/src/main/java/calculation/Calculator.java
@@ -2,8 +2,14 @@ package calculation;
 
 public class Calculator {
     public int add(String numbersAsString) {
+        if (numbersAsString == null) {
+            throw new IllegalArgumentException("Null not acceptable!");
+        }
         if ("".equals(numbersAsString)) return 0;
-        if ("3,4".equals(numbersAsString)) return 7;
-        return Integer.valueOf(numbersAsString);
+        String[] splitted = numbersAsString.split(",");
+        if (splitted.length == 1) {
+            return Integer.valueOf(numbersAsString);
+        }
+        return Integer.parseInt(splitted[0]) + Integer.parseInt(splitted[1]);
     }
 }
