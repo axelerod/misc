78eaa75eb8fb26cb5a2f9431bbd5e9d1234ba6e5
Oleksii Burov <aburov@smartling.com>
2015-11-18 18:33:25 +0200

    REF: renaming

--- untitled/approach/tdd/calculator/src/main/java/calculation/CustomSplitter.java
+++ untitled/approach/tdd/calculator/src/main/java/calculation/CustomSplitter.java
@@ -14,21 +14,29 @@ class CustomSplitter extends Splitter {
         String customDelimiter = delimiterAndNumbers.substring(2, newLineIndex);
 
         if (delimiterInBrackets(customDelimiter)) {
-            Pattern pattern = Pattern.compile("(\\[.+?\\])+?");
-            Matcher matcher = pattern.matcher(customDelimiter);
-            matcher.find();
-            String group = matcher.group();
-            customDelimiter = group.substring(1, group.length() -1);
-            while (matcher.find()) {
-                String group1 = matcher.group();
-                customDelimiter = customDelimiter + "|" +group1.substring(1, group1.length() -1);
-            }
+            customDelimiter = buildCustomDelimiters(customDelimiter);
         }
 
         String effectiveDelimiters = DEFAULT_DELIMITERS + "|" + customDelimiter;
         String delimitedNumbers = delimiterAndNumbers.substring(newLineIndex + 1);
 
         return delimitedNumbers.split(effectiveDelimiters);
+    }
+
+    private String buildCustomDelimiters(String customDelimiter) {
+        Pattern pattern = Pattern.compile("(\\[.+?\\])+?");
+        Matcher matcher = pattern.matcher(customDelimiter);
+        StringBuilder delimitersCollector = new StringBuilder("");
+
+        matcher.find();
+        String firstMatchedGroup = matcher.group();
+        delimitersCollector.append(firstMatchedGroup.substring(1, firstMatchedGroup.length() - 1));
+
+        while (matcher.find()) {
+            String anotherMatchedGroup = matcher.group();
+            delimitersCollector.append("|").append(anotherMatchedGroup.substring(1, anotherMatchedGroup.length() - 1));
+        }
+        return delimitersCollector.toString();
     }
 
     private boolean delimiterInBrackets(String customDelimiter) {
