f23b0c73e90fe1ee74ea46ed2d2bf9b259418ea6
Oleksii Burov <aburov@smartling.com>
2015-11-18 17:13:03 +0200

    REF: extracted method.

--- untitled/approach/tdd/calculator/src/main/java/calculation/CustomSplitter.java
+++ untitled/approach/tdd/calculator/src/main/java/calculation/CustomSplitter.java
@@ -10,12 +10,17 @@ class CustomSplitter extends Splitter {
         int newLineIndex = delimiterAndNumbers.indexOf("\n");
         String customDelimiter = delimiterAndNumbers.substring(2, newLineIndex);
 
-        if (customDelimiter.startsWith("[") && customDelimiter.endsWith("]")) {
-            customDelimiter = customDelimiter.substring(1, customDelimiter.length() -1);
+        if (delimiterInBrackets(customDelimiter)) {
+            customDelimiter = customDelimiter.substring(1, customDelimiter.length() - 1);
         }
+
         String effectiveDelimiters = DEFAULT_DELIMITERS + "|" + customDelimiter;
         String delimitedNumbers = delimiterAndNumbers.substring(newLineIndex + 1);
 
         return delimitedNumbers.split(effectiveDelimiters);
+    }
+
+    private boolean delimiterInBrackets(String customDelimiter) {
+        return customDelimiter.startsWith("[") && customDelimiter.endsWith("]");
     }
 }
