1246729ff0e0a29dadc5443c69762b68bfef6d90
Oleksii Burov <aburov@smartling.com>
2015-11-18 11:34:30 +0200

    REF: extracted constant. Renamed variables.

--- untitled/approach/tdd/calculator/src/main/java/calculation/Calculator.java
+++ untitled/approach/tdd/calculator/src/main/java/calculation/Calculator.java
@@ -4,28 +4,26 @@ import java.util.Arrays;
 import java.util.stream.StreamSupport;
 
 public class Calculator {
+
+    public static final String DEFAULT_DELIMITERS = ",|\\v";
+
     public int add(String delimiterAndNumbers) {
         if (delimiterAndNumbers == null)
             throw new IllegalArgumentException("Null not acceptable!");
 
         if (delimiterAndNumbers.isEmpty()) return 0;
 
-        String defaultDelimiters = ",|\\v";
-        String effectiveDelimiters = defaultDelimiters;
-        String oneCharDelimiter = "";
+        String effectiveDelimiters = DEFAULT_DELIMITERS;
         String delimitedNumbers = delimiterAndNumbers;
+
         if (delimiterAndNumbers.contains("//")) {
-            oneCharDelimiter = delimiterAndNumbers.substring(2,3);
-            effectiveDelimiters = defaultDelimiters + "|" + oneCharDelimiter;
+            String customDelimiter = delimiterAndNumbers.substring(2,3);
+            effectiveDelimiters = DEFAULT_DELIMITERS + "|" + customDelimiter;
             delimitedNumbers = delimiterAndNumbers.substring(4);
         }
 
         String[] splitted = delimitedNumbers.split(effectiveDelimiters);
 
         return StreamSupport.stream(Arrays.spliterator(splitted), false).mapToInt(Integer::parseInt).reduce(0, (a, b) -> a += b);
-    }
-
-    private int toInt(String s) {
-        return Integer.parseInt(s);
     }
 }
