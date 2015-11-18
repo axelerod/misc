464bcda5b202fe11f4431a19fc1186657ea9d7b1
Oleksii Burov <aburov@smartling.com>
2015-11-18 16:55:40 +0200

    GREEN: negative not allowed implementation

--- untitled/approach/tdd/calculator/src/main/java/calculation/Calculator.java
+++ untitled/approach/tdd/calculator/src/main/java/calculation/Calculator.java
@@ -3,6 +3,8 @@ package calculation;
 import java.util.Arrays;
 import java.util.stream.StreamSupport;
 
+import static java.util.stream.Collectors.toList;
+
 public class Calculator {
 
     public int add(String delimiterAndNumbers) {
@@ -12,6 +14,12 @@ public class Calculator {
         if (delimiterAndNumbers.isEmpty()) return 0;
 
         String[] splitted = split(delimiterAndNumbers);
+
+        int[] negativeNumbers = StreamSupport.stream(Arrays.spliterator(splitted), false).mapToInt(Integer::parseInt).filter(a -> a < 0).toArray();
+
+        if (negativeNumbers.length > 0) {
+            throw new IllegalArgumentException("Negatives are not allowed: " + negativeNumbers);
+        }
 
         return StreamSupport.stream(Arrays.spliterator(splitted), false).mapToInt(Integer::parseInt).reduce(0, (a, b) -> a += b);
     }
